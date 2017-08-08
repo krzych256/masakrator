e1035555e6b7f3b7f22a60adc4548ca0f0a6ff57
krzych256 <jasio93z@gmail.com>
2017-08-08 17:11:56 +0200

    Update and rename KindOfSoldier.java to SoldierType.java

--- src/main/java/com/game/masakrator/model/SoldierType.java
+++ src/main/java/com/game/masakrator/model/SoldierType.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class KindOfSoldier {
+public class SoldierType {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
