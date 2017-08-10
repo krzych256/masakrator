3c5b68525eb3fb8edc7da462d0e4e1a93f0cb2a9
krzysiek <krzysztof@EWADL02DHLVVZ1.ams.com>
2017-08-10 11:33:07 +0200

    Configuration

--- src/main/java/com/game/masakrator/controller/MainController.java
+++ src/main/java/com/game/masakrator/controller/MainController.java
@@ -1,20 +1,40 @@
 package com.game.masakrator.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.game.masakrator.dao.UserRepository;
+import com.game.masakrator.model.User;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
-	@RequestMapping("/")
+	//@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
 		return "index";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> getUser() {
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void addUser(@RequestBody User user) {
+		userRepository.save(user);
 	}
 }
--- src/main/resources/frondend-view/src/app/app.module.ts
+++ src/main/resources/frondend-view/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { UserService } from './services/user.service';
+
+const appRoutes: Routes = [
+  {path:'', component:GameComponent},
+  {path:'about', component:GameComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
