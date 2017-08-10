9a6106abc051af10316144054c232fdbf88d6195
krzysiek <krzysztof>
2017-08-10 16:09:43 +0200

    registration and login

--- src/main/resources/frondend-view/src/app/app.component.ts
+++ src/main/resources/frondend-view/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -8,3 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
 }
+
--- src/main/resources/frondend-view/src/app/app.module.ts
+++ src/main/resources/frondend-view/src/app/app.module.ts
@@ -2,33 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { GameComponent } from './components/game/game.component';
-import { LoginComponent } from './components/login/login.component';
-//import { User } from './components/model/user';
+// used to create fake backend
+import { fakeBackendProvider } from './components/fakeBackend/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
 
+import { AppComponent }  from './app.component';
+import { routing } from './app.routing';
+ 
+import { AlertComponent } from './components/alert/alert.component';
+import { AuthGuard } from './components/guards/auth.guard';
+import { AlertService } from './services/alert.service';
+import { AuthenticationService } from './services/authentication.service';
 import { UserService } from './services/user.service';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const appRoutes: Routes = [
-  {path:'', component:GameComponent},
-  {path:'login', component:LoginComponent}
-];
+import { GameComponent } from './components/game/game.component';
+//import { LoginComponent2 } from './components/login/login.component';
+
+//import { UserService2 } from './services/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    LoginComponent
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent   
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    routing
   ],
-  providers: [UserService],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
